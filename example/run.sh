2131d901851c1f6c91a6d8584e9a9c3f80ba454a
Guillaume Bouvignies <gbouvignies@gmail.com>
2016-11-25 19:10:59 +0100

    initial commit

---
+++ example/run.sh
@@ -0,0 +1,7 @@
+peakfit  \
+    -s pseudo3d.ft2 \
+    -l pseudo3d.list \
+    -z b1_offsets.txt \
+    -t 1e5 \
+    -n 12139.0 \
+    -o Fits
---
+++ example/run_mc.sh
@@ -0,0 +1,8 @@
+peakfit  \
+    -s pseudo3d.ft2 \
+    -l pseudo3d.list \
+    -z b1_offsets.txt \
+    -t 8e4 \
+    -n 12139.0 \
+    --mc 9.7ppm 10.5ppm 113ppm 128ppm  5 \
+    -o FitsMC
