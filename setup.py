dd5b2766aa47217bf66e4ce54437dbff2687d56c
Guillaume Bouvignies <gbouvignies@gmail.com>
2018-09-29 15:16:56 +0200

    Prepare for CI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 import versioneer
 
-with open("Readme.md") as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
---
+++ tests/test_peakfit.py
@@ -0,0 +1,10 @@
+"""Unit and regression test for the chemex package."""
+
+import sys
+
+import peakfit
+
+
+def test_chemex_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "peakfit" in sys.modules
