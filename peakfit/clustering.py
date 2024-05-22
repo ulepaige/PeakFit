01208448eb9b6502dc13cd40a42ff320c1368eb0
Guillaume Bouvignies <guillaume.bouvignies@ens.psl.eu>
2024-05-22 23:15:06 +0200

    Consider all planes to select points above the threshold

--- peakfit/clustering.py
+++ peakfit/clustering.py
@@ -1,15 +1,33 @@
-import collections as cl
 import itertools as it
+from typing import NamedTuple
 
 import numpy as np
+from nmrglue.fileio.fileiobase import unit_conversion
+
+from peakfit.messages import print_segmenting
 
 
-Spectra = cl.namedtuple("Spectra", "data ucx ucy z_values")
-Peak = cl.namedtuple("Peak", "name x0 y0")
-Cluster = cl.namedtuple("Cluster", "peaks x y data")
+class Spectra(NamedTuple):
+    data: np.ndarray
+    ucx: unit_conversion
+    ucy: unit_conversion
+    z_values: np.ndarray
 
 
-def _merge_clusters(cluster1, cluster2):
+class Peak(NamedTuple):
+    name: str
+    x0: int
+    y0: int
+
+
+class Cluster(NamedTuple):
+    peaks: list
+    x: np.ndarray
+    y: np.ndarray
+    data: np.ndarray
+
+
+def _merge_clusters(cluster1: Cluster, cluster2: Cluster) -> Cluster:
     peak_cluster = [*cluster1.peaks, *cluster2.peaks]
     x_cluster = np.hstack((cluster1.x, cluster2.x))
     y_cluster = np.hstack((cluster1.y, cluster2.y))
@@ -17,7 +35,7 @@ def _merge_clusters(cluster1, cluster2):
     return Cluster(peak_cluster, x_cluster, y_cluster, data_cluster)
 
 
-def _distance(cluster1, cluster2, spectra):
+def _distance(cluster1: Cluster, cluster2: Cluster, spectra: Spectra) -> float:
     ucx, ucy = spectra.ucx, spectra.ucy
     distances2 = [
         (ucx.hz(ucx.f(peak1.x0, "ppm")) - ucx.hz(ucx.f(peak2.x0, "ppm"))) ** 2
@@ -28,7 +46,11 @@ def _distance(cluster1, cluster2, spectra):
     return np.sqrt(min(distances2))
 
 
-def _merge_close_clusters(clusters, spectra, cutoff):
+def _merge_close_clusters(
+    clusters: list[Cluster],
+    spectra: Spectra,
+    cutoff: float,
+) -> list[Cluster]:
     grouped = True
     while grouped:
         grouped = False
@@ -43,25 +65,38 @@ def _merge_close_clusters(clusters, spectra, cutoff):
     return clusters
 
 
-def _flood(x_start: int, y_start: int, spectra: Spectra, threshold: float) -> list:
-    ny, nx = spectra.data.shape[1:]
+def _flood(
+    x_start: int,
+    y_start: int,
+    spectra: Spectra,
+    threshold: float,
+) -> list[tuple[int, int]]:
+    data = spectra.data
+    ny, nx = data.shape[1:]
     stack = [(x_start, y_start)]
     points = set()
     while stack:
         x, y = stack.pop()
-        above = abs(spectra.data[0, y % ny, x % nx]) >= threshold
+        above = any(abs(data[:, y % ny, x % nx]) >= threshold)
         unvisited = (x, y) not in points
         if above and unvisited:
             points.add((x, y))
             stack.extend([(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])
     if len(points) < 9:
-        points = zip(range(x_start - 1, x_start + 2), range(y_start - 1, y_start + 2))
+        points = zip(
+            range(x_start - 1, x_start + 2),
+            range(y_start - 1, y_start + 2),
+            strict=False,
+        )
     return list(points)
 
 
-def cluster_peaks(spectra: Spectra, peaks: np.ndarray, contour_level: float) -> list:
-
-    print("- Segmenting the spectra and clustering the peaks...")
+def cluster_peaks(
+    spectra: Spectra,
+    peaks: np.ndarray,
+    contour_level: float,
+) -> list[Cluster]:
+    print_segmenting()
 
     nz, ny, nx = spectra.data.shape
 
@@ -69,7 +104,9 @@ def cluster_peaks(spectra: Spectra, peaks: np.ndarray, contour_level: float) -> 
 
     x_pts = np.rint(spectra.ucx.f(peaks["x"], "ppm")).astype(int)
     y_pts = np.rint(spectra.ucy.f(peaks["y"], "ppm")).astype(int)
-    peaks_pt = {name: (x, y) for name, x, y in zip(peaks["names"], x_pts, y_pts)}
+    peaks_pt = {
+        name: (x, y) for name, x, y in zip(peaks["names"], x_pts, y_pts, strict=False)
+    }
 
     names = set(peaks_pt)
 
